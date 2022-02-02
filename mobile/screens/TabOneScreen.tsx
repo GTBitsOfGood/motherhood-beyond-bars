c3b0307ede3c3823ff3084189fd1277fac538938
Patrick Hultquist <patrick@monogram.io>
2022-02-01 19:43:03 -0500

    add configuration

--- mobile/screens/TabOneScreen.tsx
+++ mobile/screens/TabOneScreen.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+import React from "react";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
