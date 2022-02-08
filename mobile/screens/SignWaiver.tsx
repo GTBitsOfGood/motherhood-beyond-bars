7ddf25f13732eedbe51d36d7eb0d2ae7993771ed
Patrick Hultquist <patrick@monogram.io>
2022-02-08 13:33:23 -0500

    sign multiple waivers

--- mobile/screens/SignWaiver.tsx
+++ mobile/screens/SignWaiver.tsx
@@ -1,78 +1,108 @@
-import { ScrollView, ScrollViewComponent, StyleSheet, TextInput } from "react-native";
-
-import EditScreenInfo from "../components/EditScreenInfo";
+import { StyleSheet, TextInput, Switch, ScrollView } from "react-native";
 import { Text, View } from "../components/Themed";
-import { RootTabScreenProps } from "../types";
-import { Button, CheckBox } from "react-native";
-import {
-  GoogleAuthProvider,
-  signInWithPopup,
-  signInWithRedirect,
-} from "firebase/auth";
+import { OnboardingStackScreenProps } from "../types";
+import { Button } from "react-native";
 import { auth, db } from "../config/firebase";
-import React, { useEffect, useState } from "react";
-import { collection, doc, getDocs, query, setDoc, where, updateDoc, serverTimestamp } from "firebase/firestore"; 
-import { Input } from "@mui/material";
-import { borderBottom } from "@mui/system";
+import React, { useContext, useEffect, useState } from "react";
+//@ts-ignore
+import { MarkdownView } from "react-native-markdown-view";
 
-function nextButton() {
-  console.log('Next Page');
-}
+import {
+  doc,
+  updateDoc,
+  arrayUnion,
+  getDoc,
+  Timestamp,
+} from "firebase/firestore";
+import { UserContext } from "../providers";
 
 export default function SignWaiver({
   navigation,
-}: RootTabScreenProps<"SignWaiver">) {
+  route,
+}: OnboardingStackScreenProps<"SignWaiver">) {
+  const { waiverStack, index } = route.params;
+  const waiver = waiverStack[index];
 
-  const [waiver, setWaiver] = useState('');
-  const [id, setWaiverID] = useState('');
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
+  const authData = useContext(UserContext);
+  const [isSelected, setSelection] = useState(false);
 
-  async function setSignedWaivers(name: string) {
-    const waivers = collection(db, "caregivers");
-    const q = doc(db, 'caregivers', name)
+  async function setSignedWaivers() {
+    const caregiverDoc = doc(db, "caregivers", authData?.uid as string);
 
-    const updateTimestamp = await updateDoc(q, {
-        signedWaivers: [{
-          id: id,
-          timestamp: serverTimestamp()
-        }]
+    updateDoc(caregiverDoc, {
+      signedWaivers: arrayUnion({
+        id: waiver.id,
+        timestamp: Timestamp.now(),
+      }),
     });
   }
 
-  useEffect(() => {
-    const waivers = collection(db, "waivers");
-    const q = query(waivers, where("name", "==", "Test Waiver"))
-    const waiverDocs = getDocs(q).then((res) => {
-      res.forEach(doc => {
-        setWaiver(doc.data().content.toString());
-      })
-    })
-  })
-
-  const [isSelected, setSelection] = useState(false);
-
-
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Liability Waiver</Text>
-      <TextInput style={styles.textInput} value = {waiver} editable = {false}/>
+      {/* <TextInput style={styles.textInput} value={waiver} editable={false} /> */}
+      <ScrollView
+        style={{
+          backgroundColor: "#f5f5f5",
+          borderRadius: 10,
+          padding: 20,
+          maxHeight: "60%",
+        }}
+      >
+        <MarkdownView
+          styles={{
+            heading1: {
+              fontSize: 24,
+            },
+          }}
+        >
+          {waiver.content}
+        </MarkdownView>
+      </ScrollView>
 
-      <View style={[styles.container, {
-        flexDirection: "row"
-      }]}>
-        <CheckBox value={isSelected}
-          onValueChange={setSelection}/>
-        <Text style={styles.agree}>I agree to the Liability Waiver</Text>
+      <View
+        style={[
+          styles.container,
+          {
+            flexDirection: "row",
+          },
+        ]}
+      >
+        {/* <CheckBox value={isSelected} onValueChange={setSelection} /> */}
       </View>
-  
-      <Text style={styles.textLabel}>Signature</Text>
-      <TextInput style={styles.sign} onChangeText={setName}/>
+
+      <Switch
+        value={isSelected}
+        onChange={() => {
+          setSelection(!isSelected);
+        }}
+      />
+      <Text style={styles.agree}>I agree to the Liability Waiver</Text>
+      {/* <Text style={styles.textLabel}>Signature</Text>
+      <TextInput style={styles.sign} onChangeText={setName} />
       <Text style={styles.textLabel}>Date</Text>
-      <TextInput style={styles.sign}/>
-      <Button 
-        title={'Next'}
-        onPress={() => setSignedWaivers}
-        style={[styles.button]}
+      <TextInput style={styles.sign} /> */}
+      <Button
+        title={"Next"}
+        onPress={() => {
+          if (isSelected) {
+            console.log("Signed waiver", waiver.id);
+
+            setSignedWaivers();
+
+            if (waiverStack.length > index + 1) {
+              navigation.push("SignWaiver", {
+                waiverStack,
+                index: index + 1,
+              });
+            } else {
+              alert("All Waivers Signed!");
+            }
+          } else {
+            alert("You must agree to the liability waiver before continuing.");
+          }
+        }}
+        // style={[styles.button]}
       />
     </View>
   );
@@ -80,9 +110,9 @@ export default function SignWaiver({
 
 const styles = StyleSheet.create({
   agree: {
-    fontSize: 15, 
+    fontSize: 15,
     fontWeight: "bold",
-    paddingLeft: 15
+    paddingLeft: 15,
   },
   container: {
     flex: 1,
@@ -91,7 +121,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: "bold",
-    paddingBottom: 15
+    paddingBottom: 15,
   },
   separator: {
     marginVertical: 30,
@@ -99,29 +129,29 @@ const styles = StyleSheet.create({
     width: "80%",
   },
   textInput: {
-      height: "50%",
-      width: "100%",
-      borderWidth: 1,
-      padding: 5,
-      borderColor: 'black',
+    height: "50%",
+    width: "100%",
+    borderWidth: 1,
+    padding: 5,
+    borderColor: "black",
   },
   sign: {
     height: "5%",
     width: "100%",
     borderWidth: 1,
-    borderColor: 'black',
+    borderColor: "black",
     paddingLeft: 5,
-},
+  },
   textLabel: {
-      fontWeight: "bold",
-      fontSize: 18,
-      paddingBottom: 5,
-      paddingTop: 15
+    fontWeight: "bold",
+    fontSize: 18,
+    paddingBottom: 5,
+    paddingTop: 15,
   },
   button: {
-    borderColor: 'black',
+    borderColor: "black",
     borderWidth: 1,
-    width: '100%',
-    paddingTop: 30
-  }
+    width: "100%",
+    paddingTop: 30,
+  },
 });
