cf5aa1669bec3903370265b01ba20c44a94d399c
Patrick Hultquist <patrick@monogram.io>
2022-03-14 20:53:32 -0400

    fix more linting errors

--- web/pages/caretakers.tsx
+++ web/pages/caretakers.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 function genCaretakersTab() {
-    return (
-        <div>
-            <div class="absolute mt-20 border-t w-full"/>
-            <div class="pt-6 px-8 flex h-full flex-col justify-left">
-                <div class="flex flex-row">
-                    <h1 className="text-2xl mb-5 font-bold">Caretakers</h1>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="absolute mt-20 border-t w-full" />
+      <div className="pt-6 px-8 flex h-full flex-col justify-left">
+        <div className="flex flex-row">
+          <h1 className="text-2xl mb-5 font-bold">Caretakers</h1>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default genCaretakersTab;
