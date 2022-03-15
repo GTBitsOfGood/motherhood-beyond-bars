dee7a60a1babd1280b299c5c816c8fddbd3573e9
Patrick Hultquist <patrick@monogram.io>
2022-03-14 20:16:13 -0400

    use typescript eslint

---
+++ web/.eslintrc.js
@@ -0,0 +1,6 @@
+export default {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+};
