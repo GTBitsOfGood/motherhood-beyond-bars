301b970385065ba784c12beb411acfc5976f7bab
saisahitib <saisahitib@gmail.com>
2022-04-10 14:21:59 -0400

    updated settings account information UI

--- web/pages/settings.tsx
+++ web/pages/settings.tsx
@@ -10,45 +10,52 @@ type SettingsPhone = {
 
 function genSettingsTab({ phoneNumber }: SettingsPhone) {
   const [phNumber, setPhNumberState] = useState(phoneNumber);
+  const [editPhone, setEditPhone] = useState(false);
+  const [password, setPassword] = useState("passwordpasswordpasswordpasswordpasswordpassword");
+  const [editPassword, setEditPassword] = useState(false);
   const {
     register,
     handleSubmit,
-    setValue,
     formState: { errors },
   } = useForm<SettingsPhone>();
 
-  const onSubmit = handleSubmit(async (data) => {
+  const onPhoneNumberSubmit = handleSubmit(async (data) => {
     const newNumber = data.phoneNumber;
-
-    const settingsRef = doc(db, "app", "settings");
-
-    await updateDoc(settingsRef, {
-      contact: {
-        phone: newNumber,
-      },
-    });
-
-    alert("Phone number updated");
-    setPhNumberState(newNumber);
-    setValue("phoneNumber", "");
+    if (newNumber != phNumber) {
+      const settingsRef = doc(db, "app", "settings");
+      await updateDoc(settingsRef, {
+        contact: {
+          phone: newNumber,
+        },
+      });
+      alert("Phone number updated");
+      setPhNumberState(newNumber);
+      setEditPhone(false);
+    }
   });
+
+  const onPasswordSubmit = handleSubmit(async (data) => {
+
+  })
 
   return (
     <div>
-      <div className="absolute mt-20 border-t w-full" />
+      <div className="absolute mt-20 border-t w-[1122px]" />
       <div className="pt-6 px-8 flex h-full flex-col justify-left">
         <h1 className="text-2xl mb-5 font-bold">Settings</h1>
-        <h2 className="text-md mt-5 mb-5 font-bold">
-          Current Phone Number: {phNumber}
-        </h2>
-        <h2 className="text-md mb-5 font-bold">Update Phone Number</h2>
-        <form className="w-full max-w-sm" onSubmit={onSubmit}>
-          <div className="flex items-center border-b py-2">
-            <input
+        <label className="font-bold text-lg pt-6">Account Information</label>
+        <form onSubmit={onPhoneNumberSubmit}>
+          <div className="flex items-center pt-3">
+            <label>
+            Phone Number:
+            </label>
+            {editPhone ? (
+              <div className="flex items-center">
+                <input
               id="phoneInput"
-              className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+              className="h-10 appearance-none block ml-4 bg-gray-100 text-gray-700 border rounded py-3 px-3 leading-tight focus:outline-none focus:bg-white"
               type="text"
-              placeholder="Enter Phone Number"
+              defaultValue={phNumber}
               aria-label="Phone number"
               {...register("phoneNumber", {
                 pattern:
@@ -56,18 +63,104 @@ function genSettingsTab({ phoneNumber }: SettingsPhone) {
                 required: true,
               })}
             />
-            {errors.phoneNumber && (
-              <p className="text-red-500 text-xs italic">
-                {" "}
-                {errors.phoneNumber.message}
-              </p>
-            )}
             <button
-              className="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
+              className="h-10 w-[68px] flex-shrink-0 hover:bg-blue-100 border-blue-700 text-base border text-blue-700 ml-4 rounded"
               type="submit"
             >
-              Submit
+              Save
             </button>
+            <button
+              className="h-10 w-20 flex-shrink-0 hover:text-blue-900 text-base text-blue-700"
+              onClick={()=>setEditPhone(false)}
+            >
+              Cancel
+            </button>
+              </div>
+            ) : (
+              <div className="flex items-center">
+                <p className="ml-4">{phNumber}</p>
+                <button
+                className="h-10 w-20 flex-shrink-0 hover:text-blue-900 text-base text-blue-700"
+                onClick={()=>setEditPhone(true)}
+              >
+                Edit
+              </button>
+              </div>
+            )}
+          </div>
+        </form>
+        <form onSubmit={onPasswordSubmit}>
+          <div>
+            {editPassword ?
+              (
+                <div className="pt-4">
+                  <div className="flex items-center">
+                    <label className="pr-[35px]">
+                    Password:
+                    </label>
+                      <label className="pr-[76px]">
+                      Current password
+                      </label>
+                      <label className="pr-[95px]">
+                      New password
+                      </label>
+                      <label>
+                      Confirm new password
+                      </label>
+                  </div>
+                  <div className="flex items-center pt-1">   
+                    <input
+                      className="h-10 ml-[107px] mr-3 appearance-none block bg-gray-100 text-gray-700 border rounded py-3 px-3 leading-tight focus:outline-none focus:bg-white"
+                      type="password"
+                      aria-label="Current password"
+                    />
+                    <input
+                      className="h-10 mr-3 appearance-none block bg-gray-100 text-gray-700 border rounded py-3 px-3 leading-tight focus:outline-none focus:bg-white"
+                      type="password"
+                      aria-label="New password"
+                    />
+                    <input
+                      className="h-10 appearance-none block bg-gray-100 text-gray-700 border rounded py-3 px-3 leading-tight focus:outline-none focus:bg-white"
+                      type="password"
+                      aria-label="Confirm new password"
+                    />
+                    <button
+                      className="h-10 w-[68px] flex-shrink-0 hover:bg-blue-100 border-blue-700 text-base border text-blue-700 ml-4 rounded"
+                      type="submit"
+                    >
+                      Save
+                    </button>
+                    <button
+                      className="h-10 w-20 flex-shrink-0 hover:text-blue-900 text-base text-blue-700"
+                      onClick={()=>setEditPassword(false)}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              ) : (
+                <div className="flex items-center pt-2">
+                  <label className="pr-[54px]">
+                  Password:
+                  </label>
+                  <div className="flex items-center">
+                      <input
+                        className="bg-white w-[116px] mr-5"
+                        type="password"
+                        value={password}
+                        disabled
+                        aria-label="password"
+                      />
+                      <button
+                      className="h-10 flex-shrink-0 hover:text-blue-900 text-base text-blue-700"
+                      onClick={()=>setEditPassword(true)}
+                    >
+                      Edit
+                    </button>
+                  </div>
+                </div>
+              )
+            }
           </div>
         </form>
       </div>
