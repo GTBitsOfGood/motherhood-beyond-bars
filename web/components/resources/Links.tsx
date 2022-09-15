76425ae81b499517974cf5dbbae819fe2826f342
Mahesh Natamai <44304651+MaheshNat@users.noreply.github.com>
2022-09-15 15:04:43 -0400

    created firebase CRUD methods for links

--- web/components/resources/Links.tsx
+++ web/components/resources/Links.tsx
@@ -1,5 +1,58 @@
-import React from "react";
+import { db } from '@lib/firebase';
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from 'firebase/firestore';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 
-export default function Links() {
-  return <div>Links section here!</div>;
+type Link = {
+  id: string;
+  title: string;
+  description: string;
+  url: string;
+};
+
+export default function Links(props: { links: Link[] }) {
+  const [links, setLinks] = useState<Link[]>(props.links);
+  const router = useRouter();
+
+  const refreshData = () => {
+    router.replace(router.asPath);
+  };
+
+  const createLink = async (link: any) => {
+    await addDoc(collection(db, 'links'), {
+      ...link,
+      createdAt: serverTimestamp(),
+    });
+
+    refreshData();
+  };
+
+  const deleteLink = async (id: string) => {
+    await deleteDoc(doc(db, 'links', id));
+    setLinks(links.filter((l) => l.id !== id));
+    refreshData();
+  };
+
+  const updateLink = async (
+    id: any,
+    title: string,
+    description: string,
+    url: string
+  ) => {
+    await updateDoc(doc(db, 'links', id), {
+      title,
+      description,
+      url,
+    });
+    refreshData();
+  };
+
+  return <>links go here</>;
 }
---
+++ web/pages/resource-library.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { useState } from 'react';
+import { FAQ, Links, Research } from '../components';
+import { db } from '@lib/firebase';
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  serverTimestamp,
+} from 'firebase/firestore';
+import { GetServerSideProps } from 'next';
+
+type Link = {
+  id: string;
+  title: string;
+  description: string;
+  url: string;
+};
+
+function ResourceLibraryPage(props) {
+  const [selectedSectionIndex, setSelectedSectionIndex] = useState(0);
+
+  const sections = [
+    {
+      title: 'FAQ',
+      component: <FAQ />,
+    },
+    {
+      title: 'Links',
+      component: <Links links={props.links} />,
+    },
+    {
+      title: 'Research',
+      component: <Research />,
+    },
+  ];
+
+  return (
+    <div className="w-full">
+      <div className="flex flex-row items-center py-6 border-b w-full px-10">
+        <h1 className="text-2xl font-bold w-full">Resource Library</h1>
+      </div>
+      <section className="px-10">
+        <div className="border-b flex gap-x-1 mt-8 w-full">
+          {/* Segmented Control */}
+          {sections.map((section, i) => (
+            <button
+              className={`py-4 px-6 font-medium rounded-t-md transition-colors border translate-y-px ${
+                selectedSectionIndex === i
+                  ? 'bg-blue-700 text-white'
+                  : 'bg-gray-100 text-gray-400'
+              }`}
+              onClick={() => setSelectedSectionIndex(i)}
+            >
+              {section.title}
+            </button>
+          ))}
+        </div>
+        <>{sections[selectedSectionIndex].component}</>
+      </section>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const itemsRef = query(collection(db, 'links'));
+  const linkDocs = await getDocs(itemsRef);
+
+  const links: Link[] = [];
+
+  linkDocs.forEach(async (doc) => {
+    const data = doc.data();
+    links.push({
+      id: doc.id,
+      title: data.title,
+      description: data.description,
+      url: data.url,
+    });
+  });
+
+  return { props: { links } };
+};
+
+export default ResourceLibraryPage;
