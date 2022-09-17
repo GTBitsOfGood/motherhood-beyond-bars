289591bb13f76fea3f23a1c7eecc62b48977a11c
EmilyAL001 <70612063+EmilyAL001@users.noreply.github.com>
2022-09-17 17:50:01 -0400

    Fix to Save Changes

--- web/components/resources/FAQ.tsx
+++ web/components/resources/FAQ.tsx
@@ -3,34 +3,17 @@ import { BiTrashAlt } from "react-icons/bi";
 import { RiArrowDownSLine, RiArrowUpSLine } from "react-icons/ri";
 import { doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "@lib/firebase";
+import { getStaticProps } from "pages/waivers";
 
 
 export default function FAQ() {
-  // const [faqs, setFaqs] = useState([{ question: '', answer: '' }]);
-
-  // return (
-  //   <div>
-  //     {faqs.map(faq => <FaqQuestionAnswer faq={faq} />)}
-  //     <button type="submit" onClick={() => {
-  //       setFaqs([...faqs, { question: '', answer: '' }])
-  //     }}>+ Add a question</button>
-  //     <br>
-  //     </br>
-  //     {/* <button type="submit" onSubmit={saveChanges}>Save Changes</button> */}
-  //   </div >
-  // );
-  const [question, setQuestion] = useState('');
-  const [answer, setAnswer] = useState('');
-  const [faqs, setFaqs] = useState([{ question, answer }]);
-  // const [qas, setQAS] = useState([question, answer]);
+  const [faqs, setFaqs] = useState([{ question: '', answer: '' }]);
 
   useEffect(() => {
     let ignore = false;
     getDoc(doc(db, "resources/faq")).then((doc) => {
       if (!ignore) {
-        setQuestion(doc?.data()?.question);
-        setAnswer(doc?.data()?.answer);
-        // setFaqs(doc?.data()?.faqs);
+        setFaqs(doc?.data()?.faqs);
       }
     });
     return () => {
@@ -38,152 +21,80 @@ export default function FAQ() {
     };
   }, []);
 
-  async function setQA() {
+  async function saveChanges() {
     const qa = doc(db, "resources", "faq");
-    setDoc(qa, {
-      question: question,
-      answer: answer,
-      // faqs: faqs,
+    await updateDoc(qa, {
+      faqs: faqs
     });
-
-    // await updateDoc(qa, {
-    // faqs: { question: question, answer: answer }
-    //});
   }
+
   return (
     <div>
+      {faqs.map((faq, i) => <FaqQuestionAnswer faq={faq} setQuestion={(value) => {
+        setFaqs(faqs.map((_, i2) => {
+          return i2 === i ? { ...faq, question: value } : _
+        }))
+      }}
+        setAnswer={(value) => {
+          setFaqs(faqs.map((_, i2) => {
+            return i2 === i ? { ...faq, answer: value } : _
+          }))
+        }} />)}
+      <button type="submit" onClick={() => {
+        setFaqs([...faqs, { question: '', answer: '' }])
+      }}>+ Add a question</button>
       <br>
       </br>
-      <form >
-        {/* Q, A, and icons */}
-        <div>
-          {/* Q and A */}
-          <div>
-            {/* Q and text area*/}
-            <div>
-              <label htmlFor="question">Q</label>
-              <input className="font-opensans text-base w-80 bg-light-gray-300 border-1 border-light-gray-700 p-2"
-                type="textarea" id="question" name="question" value={question}
-                onChange={(e) => setQuestion(e.target.value)} required />
-              {/* (e) => setQuestion(e.target.value) */}
-            </div>
-            {/* A and text area */}
-            <div>
-              <label htmlFor="answer">A</label>
-              <input className="font-opensans text-base w-80 bg-light-gray-100 border-1 border-light-gray-700 p-2"
-                type="textarea" id="answer" name="answer" value={answer}
-                onChange={(e) => setAnswer(e.target.value)} required />
-            </div>
-          </div>
-          <ol className="arrows">
-            <li>
-              <button type="button">
-                <RiArrowUpSLine />
-              </button>
-            </li>
-            <li>
-              <button type="button">
-                <RiArrowDownSLine />
-              </button>
-            </li>
-            <li>
-              <button type="button">
-                <BiTrashAlt />
-              </button>
-            </li>
-          </ol>
-        </div>
-        <button type="submit"
-          onClick={() => {
-            setFaqs([...faqs, { question, answer }])
-          }}
-        >
-          + Add a question
-        </button>
-        <br>
-        </br>
-        <button type="submit" onClick={setQA}>Save Changes</button>
-        <br></br>
-      </form>
+      <button type="submit" onClick={saveChanges}>Save Changes</button>
     </div >
   );
+
+  function FaqQuestionAnswer(props) {
+    return (
+      <div>
+        <br>
+        </br>
+        <form >
+          {/* Q, A, and icons */}
+          <div>
+            {/* Q and A */}
+            <div>
+              {/* Q and text area*/}
+              <div>
+                <label htmlFor="question">Q</label>
+                <input className="font-opensans text-base w-80 bg-light-gray-300 border-1 border-light-gray-700 p-2"
+                  placeholder="Question" name="question" value={props.faq.question}
+                  onChange={(e) => props.setQuestion(e.target.value)} required />
+              </div>
+              {/* A and text area */}
+              <div>
+                <label htmlFor="answer">A</label>
+                <input className="font-opensans text-base w-80 bg-light-gray-100 border-1 border-light-gray-700 p-2"
+                  type="textarea" placeholder="Answer" name="answer" value={props.faq.answer}
+                  onChange={(e) => props.setAnswer(e.target.value)} required />
+              </div>
+            </div>
+            <ol className="arrows">
+              <li>
+                <button type="button">
+                  <RiArrowUpSLine />
+                </button>
+              </li>
+              <li>
+                <button type="button">
+                  <RiArrowDownSLine />
+                </button>
+              </li>
+              <li>
+                <button type="button">
+                  <BiTrashAlt />
+                </button>
+              </li>
+            </ol>
+          </div>
+          <br></br>
+        </form>
+      </div>
+    );
+  }
 }
-
-//   function FaqQuestionAnswer() {
-//     const [question, setQuestion] = useState('');
-//     const [answer, setAnswer] = useState('');
-//     // const [qas, setQAS] = useState([question, answer]);
-
-//     useEffect(() => {
-//       let ignore = false;
-//       getDoc(doc(db, "resources/faq")).then((doc) => {
-//         if (!ignore) {
-//           setQuestion(doc?.data()?.question);
-//           setAnswer(doc?.data()?.answer);
-//         }
-//       });
-//       return () => {
-//         ignore = true;
-//       };
-//     }, []);
-
-//     async function setQA() {
-//       const qa = doc(db, "resources", "faq");
-//       setDoc(qa, {
-//         // qa: qas
-//         question: question,
-//         answer: answer
-//       });
-//     }
-
-
-//     return (
-//       <div>
-//         <br>
-//         </br>
-//         <form >
-//           {/* Q, A, and icons */}
-//           <div>
-//             {/* Q and A */}
-//             <div>
-//               {/* Q and text area*/}
-//               <div>
-//                 <label htmlFor="question">Q</label>
-//                 <input className="font-opensans text-base w-80 bg-light-gray-300 border-1 border-light-gray-700 p-2"
-//                   type="textarea" id="question" name="question" value={question}
-//                   onChange={(e) => setQuestion(e.target.value)} required />
-//                 {/* (e) => setQuestion(e.target.value) */}
-//               </div>
-//               {/* A and text area */}
-//               <div>
-//                 <label htmlFor="answer">A</label>
-//                 <input className="font-opensans text-base w-80 bg-light-gray-100 border-1 border-light-gray-700 p-2"
-//                   type="textarea" id="answer" name="answer" value={answer}
-//                   onChange={(e) => setAnswer(e.target.value)} required />
-//               </div>
-//             </div>
-//             <ol className="arrows">
-//               <li>
-//                 <button type="button">
-//                   <RiArrowUpSLine />
-//                 </button>
-//               </li>
-//               <li>
-//                 <button type="button">
-//                   <RiArrowDownSLine />
-//                 </button>
-//               </li>
-//               <li>
-//                 <button type="button">
-//                   <BiTrashAlt />
-//                 </button>
-//               </li>
-//             </ol>
-//           </div>
-//           <button type="submit" onClick={setQA}>Save Changes</button>
-//           <br></br>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
