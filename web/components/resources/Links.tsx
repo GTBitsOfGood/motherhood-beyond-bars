0c82ee788d5bca6cf34aef0097658a7e3e4efc2f
Yatharth Bhargava <yatharthbhargava@gmail.com>
2022-09-18 06:00:09 -0400

    schema update and change detection

--- web/components/resources/Links.tsx
+++ web/components/resources/Links.tsx
@@ -1,230 +1,203 @@
-import { db } from '@lib/firebase';
-import {
-  addDoc,
-  getDocs,
-  collection,
-  deleteDoc,
-  doc,
-  serverTimestamp,
-  updateDoc,
-  onSnapshot,
-  query
-} from 'firebase/firestore';
-import React, { useEffect, useState } from 'react';
-import UpChevron from '@components/Icons/UpChevron';
-import DownChevron from '@components/Icons/DownChevron';
-import TrashCan from '@components/Icons/TrashCan'
-import {AiFillWarning} from 'react-icons/ai'
+import { db } from "@lib/firebase";
+import { doc, updateDoc, onSnapshot } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import UpChevron from "@components/Icons/UpChevron";
+import DownChevron from "@components/Icons/DownChevron";
+import TrashCan from "@components/Icons/TrashCan";
+import { AiFillWarning } from "react-icons/ai";
 
 type Link = {
-  id: string;
   title: string;
   description: string;
   url: string;
-  rank: number;
-  error?:boolean
+  error?: boolean;
 };
-
-const linksRef = collection(db, "links")
 
 export default function Links() {
   const [links, setLinks] = useState<Link[]>();
-  const [deletedLinks, setDeletedLinks] = useState<Link[]>([]);
+  const [userChanges, setUserChanges] = useState<Link[]>();
+  const [changesMade, setChangesMade] = useState<boolean>(false);
 
   useEffect(() => {
-    const q = query(collection(db, "links"));
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      var tempLinks : any = [];
-      querySnapshot.forEach((doc) => {
-          var tempLink = doc.data()
-          tempLink.id = doc.id
-          tempLinks.push(tempLink);
-      });
-      tempLinks.sort(compare_rank)
-      setLinks(tempLinks)
+    const unsub = onSnapshot(doc(db, "resources", "links"), (doc) => {
+      setLinks(doc.data()?.links);
+      setUserChanges(doc.data()?.links);
     });
-  }, [])
+  }, []);
 
-  function compare_rank(a : Link, b : Link) {
-    if ( a.rank < b.rank){
-      return -1;
-    }
-    if ( a.rank > b.rank){
-      return 1;
-    }
-    return 0;
-  }
+  useEffect(() => {
+    setChangesMade(JSON.stringify(userChanges) !== JSON.stringify(links));
+  }, [userChanges]);
 
-  const createLink = async (link: any) => {
-    await addDoc(linksRef, {
-      ...link,
-      createdAt: serverTimestamp(),
-    });
+  const updateLinks = async (newLinks: any) => {
+    await updateDoc(doc(db, "resources", "links"), { links: newLinks });
   };
 
-  const deleteLink = async (id: string) => {
-    await deleteDoc(doc(db, 'links', id));
-  };
-
-  const updateLink = async (fields : any, id : string) => {
-    await updateDoc(doc(db, 'links', id), fields);
-
-  };
-
-  const moveUp = (id : string, rank : number) => {
-    if (links && rank != 0) {
-      var tempLinks = links
-      // updateLink({rank : links[rank - 1].rank + 1}, links[rank - 1].id)
-      // updateLink({rank : tempLink.rank - 1}, id)
-      tempLinks[rank].rank -= 1
-      tempLinks[rank - 1].rank += 1
-      tempLinks.sort(compare_rank)
-      setLinks([...tempLinks])
+  const moveLink = (rank: number, shift: number) => {
+    if (userChanges && rank + shift >= 0 && rank + shift < userChanges.length) {
+      var tempLinks = userChanges;
+      var tempLink = tempLinks[rank];
+      tempLinks[rank] = tempLinks[rank + shift];
+      tempLinks[rank + shift] = tempLink;
+      setUserChanges([...tempLinks]);
     }
-  }
-
-  const moveDown = (id : string, rank : number) => {
-    if (links && rank != (links.length - 1)) {
-      var tempLinks = links
-      tempLinks[rank].rank += 1
-      tempLinks[rank + 1].rank -= 1
-      tempLinks.sort(compare_rank)
-      setLinks([...tempLinks])
-    }
-  }
+  };
 
   const createTempLink = () => {
-    if (links) {
-      var tempLinks = links
-      var tempRank = links.length
+    if (userChanges) {
+      var tempLinks = userChanges;
       tempLinks.push({
-        id: '',
-        title: '',
-        description: '',
-        rank: tempRank,
-        url: ''
-      })
-      setLinks([...tempLinks])
+        title: "",
+        description: "",
+        url: "",
+      });
+      setUserChanges([...tempLinks]);
     }
-  }
-  
-  const deleteTempLink = (rank : number) => {
-    if (links) {
-      var tempLinks = links
-      var tempLink = tempLinks[rank]
-      tempLinks.splice(rank, 1)
-      if (rank != links.length) {
-        tempLinks[rank].rank -= 1
-      }
-      var tempDeletedLinks = deletedLinks
-      tempDeletedLinks.push(tempLink)
-      setDeletedLinks([...tempDeletedLinks])
-      setLinks([...tempLinks])
+  };
+
+  const deleteTempLink = (rank: number) => {
+    if (userChanges) {
+      var tempLinks = userChanges;
+      tempLinks.splice(rank, 1);
+      setUserChanges([...tempLinks]);
     }
-  }
-  
+  };
+
   const saveChanges = () => {
-    if (links) {
-      var tempLinks = links
+    if (userChanges) {
+      var tempLinks = userChanges;
       for (var i = 0; i < tempLinks.length; i++) {
         if (tempLinks[i].url == "") {
-          tempLinks[i].error = true
-          setLinks([...tempLinks])
-          return
-        } else {
-          if (tempLinks[i].id === "") {
-            createLink({
-              title: tempLinks[i].title,
-              description: tempLinks[i].description,
-              url: tempLinks[i].url,
-              rank: tempLinks[i].rank
-            })
-          } else {
-            updateLink({title: tempLinks[i].title, description: tempLinks[i].description, url: tempLinks[i].url, rank: tempLinks[i].rank}, tempLinks[i].id)
-          }
-        }        
-      }
-      
-      for (var i = 0; i < deletedLinks.length; i++) {
-        if (deletedLinks[i].id !== "") {
-          deleteLink(deletedLinks[i].id)
+          tempLinks[i].error = true;
+          setUserChanges([...tempLinks]);
+          return;
         }
       }
-      setDeletedLinks([])
     }
-  }
+    updateLinks(userChanges);
+  };
 
   return (
-    <div className='w-full'>
-      <div className='flex flex-col w-3/4'>
-        {links?.map((link) => {
-            return (
-              <div className='flex w-full py-5' key={link.id}>
-                <div className='flex flex-col w-5/6'>
-                  <div className='flex pt-2'>
-                    <div className='text-base font-semibold w-1/5 py-2'>Title</div>
-                    
-                      <input className="w-4/5 bg-[#FAFBFC] border-[#D9D9D9] border-[1px] rounded py-2 px-2 focus:outline-0 min-h-[40px]" placeholder="Title" value={link.title} onChange={(e) => {
-                          var tempLinks = links
-                          tempLinks[link.rank].title = e.target.value
-                          setLinks([...tempLinks])
-                      }}></input>
-                      
-            
+    <div className="w-full">
+      <div className="flex flex-col w-3/4">
+        {userChanges?.map((link, index) => {
+          return (
+            <div className="flex w-full py-5" key={index}>
+              <div className="flex flex-col w-5/6">
+                <div className="flex pt-2">
+                  <div className="text-base font-semibold w-1/5 py-2">
+                    Title
                   </div>
-                  <div className='flex pt-2'>
-                    <div className='text-base font-semibold w-1/5 py-2'>Description</div>
-                    <textarea className="w-4/5 bg-[#FAFBFC] border-[#D9D9D9] border-[1px] rounded py-2 px-2 focus:outline-0 min-h-[90px]" placeholder="Description" value={link.description} onChange={(e) => {
-                      var tempLinks = links
-                      tempLinks[link.rank].description = e.target.value
-                      setLinks([...tempLinks])
-                    }}></textarea>
-                  </div>
-                  <div className='flex pt-2'>
-                    <div className='text-base font-semibold w-1/5 py-2'>URL<span className='text-[#FF3939]'>*</span></div>
-                    <div className='flex flex-col w-4/5'>
-                      <input className={link.error? "w-full bg-[#FAFBFC] border-[#FF3939] border-[1px] rounded py-2 px-2 focus:outline-0 min-h-[40px]" : "w-full bg-[#FAFBFC] border-[#D9D9D9] border-[1px] rounded py-2 px-2 focus:outline-0 min-h-[40px]"} 
-                            value={link.url}
-                            onChange={(e) => {
-                                        var tempLinks = links
-                                        tempLinks[link.rank].url = e.target.value
-                                        tempLinks[link.rank].error = false
-                                        setLinks([...tempLinks])
-                                      }}
-                            placeholder="https://example.com"
-                      ></input>
-                      {link.error? <div className='text-sm text-[#FF3939] flex align-middle'><span><AiFillWarning className='fill-[#FF3939]'></AiFillWarning></span>This field is required</div> : <></>}
-                    </div>
-                  </div>
+
+                  <input
+                    className="w-4/5 bg-[#FAFBFC] border-[#D9D9D9] border-[1px] rounded py-2 px-2 focus:outline-0 min-h-[40px]"
+                    placeholder="Title"
+                    value={link.title}
+                    onChange={(e) => {
+                      var tempLinks = userChanges;
+                      tempLinks[index].title = e.target.value;
+                      setUserChanges([...tempLinks]);
+                    }}
+                  ></input>
                 </div>
-                <div className='flex flex-col w-1/6 px-4 py-1 gap-y-2 pt-2'>
-                  <div className='flex flex-col gap-y-4'>
-                    <div className="hover:cursor-pointer" onClick={() => {
-                      moveUp(link.id, link.rank)
-                    }}>
-                      <UpChevron className='fill-[#BFBFBF]'></UpChevron>
-                    </div>
-                    <div className="hover:cursor-pointer" onClick={() => {
-                      moveDown(link.id, link.rank)
-                    }}>
-                      <DownChevron className='fill-[#BFBFBF]'></DownChevron>
-                    </div>
+                <div className="flex pt-2">
+                  <div className="text-base font-semibold w-1/5 py-2">
+                    Description
                   </div>
-                  <div className='hover:cursor-pointer' onClick={() => {
-                      deleteTempLink(link.rank)
-                    }}>
-                    <TrashCan className='fill-[#BFBFBF]'></TrashCan>
+                  <textarea
+                    className="w-4/5 bg-[#FAFBFC] border-[#D9D9D9] border-[1px] rounded py-2 px-2 focus:outline-0 min-h-[90px]"
+                    placeholder="Description"
+                    value={link.description}
+                    onChange={(e) => {
+                      var tempLinks = userChanges;
+                      tempLinks[index].description = e.target.value;
+                      setUserChanges([...tempLinks]);
+                    }}
+                  ></textarea>
+                </div>
+                <div className="flex pt-2">
+                  <div className="text-base font-semibold w-1/5 py-2">
+                    URL<span className="text-[#FF3939]">*</span>
+                  </div>
+                  <div className="flex flex-col w-4/5">
+                    <input
+                      className={
+                        link.error
+                          ? "w-full bg-[#FAFBFC] border-[#FF3939] border-[1px] rounded py-2 px-2 focus:outline-0 min-h-[40px]"
+                          : "w-full bg-[#FAFBFC] border-[#D9D9D9] border-[1px] rounded py-2 px-2 focus:outline-0 min-h-[40px]"
+                      }
+                      value={link.url}
+                      onChange={(e) => {
+                        var tempLinks = userChanges;
+                        tempLinks[index].url = e.target.value;
+                        delete tempLinks[index].error;
+                        setUserChanges([...tempLinks]);
+                      }}
+                      placeholder="https://example.com"
+                    ></input>
+                    {link.error ? (
+                      <div className="text-sm text-[#FF3939] flex align-middle">
+                        <span>
+                          <AiFillWarning className="fill-[#FF3939]"></AiFillWarning>
+                        </span>
+                        This field is required
+                      </div>
+                    ) : (
+                      <></>
+                    )}
                   </div>
                 </div>
               </div>
-            )
+              <div className="flex flex-col w-1/6 px-4 py-1 gap-y-2 pt-2">
+                <div className="flex flex-col gap-y-4">
+                  <div
+                    className="hover:cursor-pointer"
+                    onClick={() => {
+                      moveLink(index, -1);
+                    }}
+                  >
+                    <UpChevron className="fill-[#BFBFBF]"></UpChevron>
+                  </div>
+                  <div
+                    className="hover:cursor-pointer"
+                    onClick={() => {
+                      moveLink(index, 1);
+                    }}
+                  >
+                    <DownChevron className="fill-[#BFBFBF]"></DownChevron>
+                  </div>
+                </div>
+                <div
+                  className="hover:cursor-pointer"
+                  onClick={() => {
+                    deleteTempLink(index);
+                  }}
+                >
+                  <TrashCan className="fill-[#BFBFBF]"></TrashCan>
+                </div>
+              </div>
+            </div>
+          );
         })}
-        
       </div>
-      <div className='relative bottom-0 w-full'>
-        <div className='flex align-middle justify-between px-4 border-t-[1px] py-4'>
-            <div className='text-[#304CD1] font-semibold hover:cursor-pointer' onClick={createTempLink}>+ Add a link</div>
-            <div className='py-2 px-3 rounded border-[#304CD1] text-[#304CD1] border-[1px] font-semibold hover:cursor-pointer' onClick={saveChanges}>Save changes</div>
+      <div className="relative bottom-0 w-full">
+        <div className="flex align-middle justify-between px-4 border-t-[1px] py-4">
+          <div
+            className="text-[#304CD1] font-semibold hover:cursor-pointer"
+            onClick={createTempLink}
+          >
+            + Add a link
+          </div>
+          <div
+            className={
+              changesMade
+                ? "py-2 px-3 rounded bg-[#304CD1] text-[#ffffff] border-[1px] font-semibold hover:cursor-pointer"
+                : "py-2 px-3 rounded border-[#304CD1] text-[#304CD1] border-[1px] font-semibold hover:cursor-pointer"
+            }
+            onClick={saveChanges}
+          >
+            Save changes
+          </div>
         </div>
       </div>
     </div>
