a7fada75a140bda16febf8dd6c59706a9eea2335
EmilyAL001 <eamanda21@gmail.com>
2022-10-02 02:04:39 -0400

    Quick change

--- mobile/screens/resources/Research.tsx
+++ mobile/screens/resources/Research.tsx
@@ -27,7 +27,7 @@ export default function Research({
   }, []);
 
   return (
-    description != '' ?
+    description.length > 0 ?
       <View style={{ height: '100%', flexDirection: "column", padding: 20 }}>
         {/* <Button
           title="Back"
