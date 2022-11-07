6d450f21ce0a9212d9d121556af6248d2c773427
Helen <helenchen200311@gmail.com>
2022-11-06 22:08:34 -0500

    shfkjs

--- functions/src/firestore/updateLinks.ts
+++ functions/src/firestore/updateLinks.ts
@@ -23,35 +23,27 @@ const updateLinks = functions.firestore
     // get open graph data
     const promises = changed.map((link: any) => {
       // TODO: validate the URL
-      return og.fetch(link.url);
+      return og.fetch(link.url).catch((err: any) => {
+        return false;
+      });
     })
 
     const data = await Promise.all(promises);
 
     // update the database
     data.forEach((ogData: any) => {
+      if (!ogData) return;
       const index = after.links.findIndex((l: any) => l.url === ogData.url);
       console.log(ogData);
       after.links[index] = {
         ...after.links[index],
-        title: ogData.title,
-        description: ogData.description,
+        title: after.links[index].title ?? ogData.title,
+        description: after.links[index].description ?? ogData.description,
         image: ogData.image,
       }
     });
 
     return change.after.ref.set(after);
-
-    // const data = before.url !== null ? await og(before.url) : null;
-
-    // // set the data in docs
-    // return change.after.ref.update({
-    //   title: after.title == null ? data?.title : after.title,
-    //   description:
-    //     after.description == null ? data?.description : after.description,
-    //   imageURL: after.imageURL == null ? data?.image.url : after.imageURL,
-    //   url: after.url == null ? data?.url : after.url,
-    // });
   });
 
 export default updateLinks;
