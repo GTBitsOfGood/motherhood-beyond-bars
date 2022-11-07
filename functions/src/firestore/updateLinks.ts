37469cd31f308813806cff7905b81ed3fb9ed813
Patrick Hultquist <patrick@monogram.io>
2022-11-06 21:23:59 -0500

    downgrade fb admin

--- functions/src/firestore/updateLinks.ts
+++ functions/src/firestore/updateLinks.ts
@@ -1,11 +1,8 @@
-import { QueryDocumentSnapshot } from "firebase-admin/firestore";
 import * as functions from "firebase-functions";
-import { Change } from "firebase-functions";
 
 const updateLinks = functions.firestore
-  .document('resources/links')
-  .onUpdate(async (change: Change<QueryDocumentSnapshot>, context) => {
-
+  .document("resources/links")
+  .onUpdate(async (change: any, context) => {
     console.log("function ran");
 
     // retrieve the previous and current value
@@ -24,7 +21,8 @@ const updateLinks = functions.firestore
     // set the data in docs
     return change.after.ref.update({
       title: after.title == null ? data?.title : after.title,
-      description: after.description == null ? data?.description : after.description,
+      description:
+        after.description == null ? data?.description : after.description,
       imageURL: after.imageURL == null ? data?.image.url : after.imageURL,
       url: after.url == null ? data?.url : after.url,
     });
