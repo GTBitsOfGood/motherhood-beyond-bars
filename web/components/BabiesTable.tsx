9dac9207c991a714ee1c7c013b036eadf9be61d3
Mahesh Natamai <44304651+MaheshNat@users.noreply.github.com>
2022-11-14 23:29:40 -0500

    fixed UI bugs in baby table dropdown, opens baby book in new tab

--- web/components/BabiesTable.tsx
+++ web/components/BabiesTable.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import { useTable } from "react-table";
-import Image from "next/image";
-import dots from "../public/dots.png";
-import book from "../public/book.svg";
-import Modal from "./Modal";
-import ChildModal from "modals/addChildModal";
+import ChildModal from 'modals/addChildModal';
+import Image from 'next/image';
+import React, { useState } from 'react';
+import { useTable } from 'react-table';
+import book from '../public/book.svg';
+import dots from '../public/dots.png';
+import Modal from './Modal';
 
 function BabiesTable({ columns, data, onEdit, caretakers, onDelete }: any) {
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
@@ -15,6 +15,7 @@ function BabiesTable({ columns, data, onEdit, caretakers, onDelete }: any) {
 
   const [editModal, toggleEditModal] = useState(false);
   const [babyData, setBabyData] = useState({});
+  const [selectedOptionsPanel, setSelectedOptionsPanel] = useState(-1);
 
   const editBaby = (data: any) => {
     toggleEditModal(true);
@@ -23,9 +24,9 @@ function BabiesTable({ columns, data, onEdit, caretakers, onDelete }: any) {
 
   return (
     <>
-      <div className="flex flex-col">
-        <div className="sm:-mx-6 lg:-mx-8">
-          <div className="inline-block sm:px-6 lg:px-8">
+      <div className='flex flex-col'>
+        <div className='sm:-mx-6 lg:-mx-8'>
+          <div className='inline-block sm:px-6 lg:px-8'>
             <div>
               <table {...getTableProps()}>
                 <thead>
@@ -33,11 +34,11 @@ function BabiesTable({ columns, data, onEdit, caretakers, onDelete }: any) {
                     <tr {...headerGroup.getHeaderGroupProps()}>
                       {headerGroup.headers.map((column) => (
                         <th
-                          scope="col"
-                          className="py-3 px-6 text-base font-normal tracking-wider text-left text-slate-500"
+                          scope='col'
+                          className='py-3 px-6 text-base font-normal tracking-wider text-left text-slate-500'
                           {...column.getHeaderProps()}
                         >
-                          {column.render("Header")}
+                          {column.render('Header')}
                         </th>
                       ))}
                     </tr>
@@ -47,47 +48,56 @@ function BabiesTable({ columns, data, onEdit, caretakers, onDelete }: any) {
                   {rows.map((row, i) => {
                     prepareRow(row);
                     return (
-                      <tr className="" {...row.getRowProps()}>
+                      <tr className='' {...row.getRowProps()}>
                         {row.cells.slice(0, -1).map((cell) => {
                           return (
                             <td
-                              className="py-4 px-6 text-base border-t font-normal text-black whitespace-nowrap"
+                              className='py-4 px-6 text-base border-t font-normal text-black whitespace-nowrap'
                               {...cell.getCellProps()}
                             >
-                              {cell.render("Cell")}
+                              {cell.render('Cell')}
                             </td>
                           );
                         })}
-                        <td className="border-t">
-                          <div className="flex flex-row">
-                            <div className="pr-2 pt-1">
+                        <td className='border-t'>
+                          <div className='flex flex-row'>
+                            <div className='pr-2 pt-1'>
                               <Image src={book} />
                             </div>
                             <a
                               href={row.cells.slice(-1)[0].value}
-                              className="text-blue-700"
+                              className='text-blue-700'
+                              target='_blank'
                             >
                               Baby Book
                             </a>
                           </div>
                         </td>
-                        <td className="border-t">
-                          <div className="p-4">
-                            <div className="group relative">
+                        <td className='border-t'>
+                          <div className='p-4'>
+                            <div className='group relative'>
                               <button>
-                                <Image src={dots} />
+                                <Image
+                                  src={dots}
+                                  onClick={() => {
+                                    if (i === selectedOptionsPanel)
+                                      setSelectedOptionsPanel(-1);
+                                    else setSelectedOptionsPanel(i);
+                                  }}
+                                />
                               </button>
                               <nav
                                 tabIndex={0}
-                                className="absolute w-[127px] rounded-b bg-white border shadow-xl right-1 mt-1 shadow-slate-200 transition-all opacity-0 group-focus-within:visible group-focus-within:opacity-100"
+                                className='absolute w-[127px] rounded-b bg-white border shadow-xl right-1 mt-1 shadow-slate-200 transition-all opacity-0 group-focus-within:visible group-focus-within:opacity-100'
+                                hidden={selectedOptionsPanel !== i}
                               >
-                                <ul className="py-1">
+                                <ul className='py-1'>
                                   <li>
                                     <div
                                       onClick={() => {
                                         editBaby(row.original);
                                       }}
-                                      className="block px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                                      className='block px-4 py-2 hover:bg-gray-100 cursor-pointer'
                                     >
                                       Edit Profile
                                     </div>
@@ -96,10 +106,10 @@ function BabiesTable({ columns, data, onEdit, caretakers, onDelete }: any) {
                                     <div
                                       onClick={() => {
                                         confirm(
-                                          "Are you sure you want to delete this baby?"
+                                          'Are you sure you want to delete this baby?'
                                         ) && onDelete(row.original);
                                       }}
-                                      className="block px-4 py-2 text-red-500 hover:bg-gray-100 cursor-pointer"
+                                      className='block px-4 py-2 text-red-500 hover:bg-gray-100 cursor-pointer'
                                     >
                                       Remove
                                     </div>
@@ -121,10 +131,10 @@ function BabiesTable({ columns, data, onEdit, caretakers, onDelete }: any) {
       <Modal
         show={editModal}
         content={
-          <div className="h-screen flex flex-col items-center justify-center bg-gray-300 overflow-hidden">
+          <div className='h-screen flex flex-col items-center justify-center bg-gray-300 overflow-hidden'>
             <ChildModal
-              header="Edit a Child"
-              buttonText="Save"
+              header='Edit a Child'
+              buttonText='Save'
               setModal={toggleEditModal}
               onSubmit={onEdit}
               values={babyData}
