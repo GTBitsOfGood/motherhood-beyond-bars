a3d4b8ff47d719e038ca3e4ed9f82b48f06119ba
Sophia <sophialin@gatech.edu>
2024-03-12 10:08:54 -0400

    Packages and metro
    I found something on Google.

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,15 +1,27 @@
-const { getDefaultConfig } = require("metro-config");
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve("react-native-svg-transformer"),
-    },
-    resolver: {
-      assetExts: assetExts.filter((ext) => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg", "jsx", "js", "ts", "tsx", "cjs"],
-    },
-  };
-})();
+const { getDefaultConfig } = require("@react-native/metro-config");
+const { mergeConfig } = require('metro-config');
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
+// module.exports = (async () => {
+//   const {
+//     resolver: { sourceExts, assetExts },
+//   } = await getDefaultConfig();
+//   return {
+//     transformer: {
+//       babelTransformerPath: require.resolve("react-native-svg-transformer"),
+//     },
+//     resolver: {
+//       assetExts: assetExts.filter((ext) => ext !== "svg",
+//       sourceExts: [...sourceExts, "svg", "jsx", "js", "ts", "tsx", "cjs"],
+//     },
+//   };
+// })();
