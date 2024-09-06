31b9dcebcd0c62a906bcbad1876a870e255db75c
tsar-boomba <itg.2048@gmail.com>
2024-09-05 20:22:07 -0400

    Use colors from tailwind config for back button

--- web/components/atoms/BackButton.tsx
+++ web/components/atoms/BackButton.tsx
@@ -7,11 +7,32 @@ interface Props {
 
 export default function BackButton({ onClick, disabled }: Props) {
 	return (
-		<button className="group flex items-center gap-[2px] text-[#8C8C8C]" onClick={onClick} disabled={disabled}>
-			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-				<path d="M15 18L9 12L15 6" className={`${disabled ? 'stroke-gray-500' : "group-hover:stroke-gray-500"}`} stroke="#8C8C8C" stroke-width="3" stroke-linecap="square" stroke-linejoin="round"/>
+		<button
+			className='group flex items-center gap-[2px] text-medium-gray'
+			onClick={onClick}
+			disabled={disabled}
+		>
+			<svg
+				xmlns='http://www.w3.org/2000/svg'
+				width='24'
+				height='24'
+				viewBox='0 0 24 24'
+				fill='none'
+			>
+				<path
+					d='M15 18L9 12L15 6'
+					className={`${disabled ? 'stroke-dark-gray' : 'group-hover:stroke-dark-gray'}`}
+					stroke='#8C8C8C'
+					stroke-width='3'
+					stroke-linecap='square'
+					stroke-linejoin='round'
+				/>
 			</svg>
-			<div className={`${disabled ? 'text-gray-500' :'group-hover:text-gray-500'}`}>Back</div>
+			<div
+				className={`${disabled ? 'text-stroke-dark-gray' : 'group-hover:text-stroke-dark-gray'}`}
+			>
+				Back
+			</div>
 		</button>
 	);
 }
