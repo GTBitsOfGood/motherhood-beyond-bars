0821689ca8236db8a995e9f6d6713759d57d31a6
Parker Abramson <parkerrabramson@gmail.com>
2024-09-05 21:49:48 -0400

    Run linter before merging

--- web/components/atoms/BackButton.tsx
+++ web/components/atoms/BackButton.tsx
@@ -1,37 +1,37 @@
 import { MouseEventHandler } from "react";
 
 interface Props {
-	onClick?: MouseEventHandler<HTMLButtonElement>;
-	disabled?: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
 }
 
 export default function BackButton({ onClick, disabled }: Props) {
-	return (
-		<button
-			className='group flex items-center gap-[2px] text-medium-gray'
-			onClick={onClick}
-			disabled={disabled}
-		>
-			<svg
-				xmlns='http://www.w3.org/2000/svg'
-				width='24'
-				height='24'
-				viewBox='0 0 24 24'
-				fill='none'
-			>
-				<path
-					d='M15 18L9 12L15 6'
-					className={`stroke-medium-gray ${disabled ? 'stroke-dark-gray' : 'group-hover:stroke-dark-gray'}`}
-					strokeWidth='3'
-					strokeLinecap='square'
-					strokeLinejoin='round'
-				/>
-			</svg>
-			<div
-				className={`${disabled ? 'text-dark-gray' : 'group-hover:text-dark-gray'}`}
-			>
-				Back
-			</div>
-		</button>
-	);
+  return (
+    <button
+      className="group flex items-center gap-[2px] text-medium-gray"
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+      >
+        <path
+          d="M15 18L9 12L15 6"
+          className={`stroke-medium-gray ${disabled ? "stroke-dark-gray" : "group-hover:stroke-dark-gray"}`}
+          strokeWidth="3"
+          strokeLinecap="square"
+          strokeLinejoin="round"
+        />
+      </svg>
+      <div
+        className={`${disabled ? "text-dark-gray" : "group-hover:text-dark-gray"}`}
+      >
+        Back
+      </div>
+    </button>
+  );
 }
