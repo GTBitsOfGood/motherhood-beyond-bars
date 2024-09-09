5c304e2feb3a8888a5e4f3060fac0efd5f20fc02
Ivan Wang <ivanwang1239@gmail.com>
2024-09-09 19:20:48 -0400

    make necessary changes for half and loading screens

--- web/components/logos/HalfScreen.tsx
+++ web/components/logos/HalfScreen.tsx
@@ -5,14 +5,11 @@ export default function HalfScreen() {
     return (
         <div className="flex flex-col bg-custom-background w-full h-[20%] justify-center items-center sm:w-1/2 sm:h-full">
             <div className="flex flex-row relative w-full h-[40%] justify-center sm:w-[17%] sm:h-[13%] sm:mb-[5%]">
-                <img src = '/MBBLogo.png' className="shrink-0 stroke-[2.1px] object-contain sm:stroke-[3px]"></img>
+                <img src = '/MBBLogo.svg' className="shrink-0 stroke-[2.1px] object-contain sm:stroke-[3px]"></img>
             </div>
             <div className="hidden sm:flex flex-col w-full h-[10%] justify-center shrink-0">
-                <p className="text-white text-center font-opensans text-[30px] font-bold leading-normal tracking-[0.9px] uppercase">
-                    Motherhood
-                </p>
-                <p className="text-white text-center font-opensans text-[30px] font-bold leading-normal tracking-[0.9px] uppercase">
-                    Beyond bars
+                <p className="text-white text-center font-opensans text-3xl font-bold tracking-wide uppercase sm:text-4xl">
+                    Motherhood <br /> Beyond bars
                 </p>
             </div>
         </div>
--- web/components/logos/Loading.tsx
+++ web/components/logos/Loading.tsx
@@ -1,18 +1,15 @@
-import React from "react"
+
 
 export default function LoadingScreen() {
 
     return (
         <div className="flex flex-col bg-custom-background w-full h-screen justify-center items-center">
             <div className="flex flex-row relative w-full h-[10%] justify-center mb-[10%] sm:w-[17%] sm:h-[13%] sm:mb-[5%]">
-                <img src = '/MBBLogo.png' className="shrink-0 stroke-[2.1px] object-contain sm:stroke-[3px]"></img>
+                <img src = '/MBBLogo.svg' className="shrink-0 stroke-[2.1px] object-contain sm:stroke-[3px]"></img>
             </div>
             <div className="flex flex-col w-full h-[10%] justify-center shrink-0">
-                <p className="text-white text-center font-opensans text-[30px] font-bold leading-normal tracking-[0.9px] uppercase">
-                    Motherhood
-                </p>
-                <p className="text-white text-center font-opensans text-[30px] font-bold leading-normal tracking-[0.9px] uppercase">
-                    Beyond bars
+                <p className="text-white text-center font-opensans text-3xl font-bold tracking-wide uppercase sm:text-4xl">
+                    Motherhood <br /> Beyond bars
                 </p>
             </div>
         </div>
