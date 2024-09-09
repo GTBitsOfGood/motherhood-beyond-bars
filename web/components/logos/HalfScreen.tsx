161c8403503dfd9274f6084c5f2b3ec7824525d5
Ivan Wang <ivanwang1239@gmail.com>
2024-09-08 20:29:57 -0400

    implement halfscreen

--- web/components/logos/HalfScreen.tsx
+++ web/components/logos/HalfScreen.tsx
@@ -1,13 +1,13 @@
 import React from "react"
 
-export default function LoadingScreen() {
-
+export default function HalfScreen() {
+    
     return (
-        <div className="flex flex-col bg-custom-background w-full h-screen justify-center items-center">
-            <div className="flex flex-row relative w-full h-[10%] justify-center mb-[10%] sm:w-[17%] sm:h-[13%] sm:mb-[5%]">
+        <div className="flex flex-col bg-custom-background w-full h-[20%] justify-center items-center sm:w-1/2 sm:h-full">
+            <div className="flex flex-row relative w-full h-[40%] justify-center sm:w-[17%] sm:h-[13%] sm:mb-[5%]">
                 <img src = '/MBBLogo.png' className="shrink-0 stroke-[2.1px] object-contain sm:stroke-[3px]"></img>
             </div>
-            <div className="flex flex-col w-full h-[10%] justify-center shrink-0">
+            <div className="hidden sm:flex flex-col w-full h-[10%] justify-center shrink-0">
                 <p className="text-white text-center font-opensans text-[30px] font-bold leading-normal tracking-[0.9px] uppercase">
                     Motherhood
                 </p>
