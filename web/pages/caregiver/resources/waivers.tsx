e2be6901f037e0c608e8fa087d1bb7357879f994
Ivan Wang <114537147+ivanwangg@users.noreply.github.com>
2024-10-20 23:40:37 -0400

    Create Resources, FAQ, and Links Pages (#161)
    * create resources, faq, and links, pages
    
    * Move files and merge styles
    
    ---------
    
    Co-authored-by: Parker Abramson <parkerrabramson@gmail.com>

--- web/pages/caregiver/resources/waivers.tsx
+++ web/pages/caregiver/resources/waivers.tsx
@@ -1,9 +1,15 @@
-import DownloadIcon from "@components/Icons/DownloadIcon";
-import { BrowserWaiver } from "@lib/types/common";
-import { getWaivers } from "db/actions/shared/Waiver";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
 import { Timestamp } from "firebase-admin/firestore";
 import MarkdownIt from "markdown-it";
-import { GetServerSideProps } from "next";
+
+import { getWaivers } from "db/actions/shared/Waiver";
+import { BrowserWaiver } from "@lib/types/common";
+
+import DownloadIcon from "@components/Icons/DownloadIcon";
+import BackButton from "@components/atoms/BackButton";
+import QuestionAnswer from "@components/resources/QuestionAnswer";
+import TopBar from "@components/resources/TopBar";
 
 interface Props {
   waivers: BrowserWaiver[];
@@ -12,30 +18,52 @@ interface Props {
 const mdParser = new MarkdownIt();
 
 export default function Waivers({ waivers }: Props) {
+  const router = useRouter();
+
   return (
-    <div className="w-full p-6 overflow-auto">
-      <h1 className="text-2xl font-bold mb-[1.125rem]">Waivers & Forms</h1>
-      <div className="flex flex-col gap-4">
-        {/* TODO: Show waivers in Accordion */}
-        {waivers.map((waiver, i) => (
-          <div key={i} className="flex flex-col p-4 gap-2 border rounded">
-            <h2 className="text-xl font-semibold">{waiver.name}</h2>
-            <div
-              className="bg-secondary-background border border-light-gray overflow-auto shrink-0 py-2 px-3 max-h-[300px]"
-              dangerouslySetInnerHTML={{
-                __html: mdParser.render(waiver.content),
-              }}
-            />
-            <button
-              className="flex gap-1 self-start items-center text-mbb-pink fill-mbb-pink font-semibold"
-              onClick={() => {
-                // TODO: Download form
-              }}
-            >
-              <span>Download Form</span> <DownloadIcon />
-            </button>
+    // TODO fix styles and formatting to alignw with Figma
+    <div className="w-full h-full flex flex-col justify-start items-start">
+      <TopBar />
+      <div className="w-full p-6 overflow-auto">
+        <BackButton
+          onClick={() => {
+            router.push("/caregiver/resources");
+          }}
+          darkerColor={true}
+        />
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-2xl font-bold mb-[1.125rem] w-full">
+            Waivers & Forms
+          </h1>
+          <div className="flex flex-col gap-4">
+            {waivers.map((waiver, i) => (
+              <QuestionAnswer
+                title={waiver.name}
+                content={
+                  <div
+                    key={i}
+                    className="flex flex-col p-4 gap-2 border rounded"
+                  >
+                    <div
+                      className="bg-secondary-background border border-light-gray overflow-auto shrink-0 py-2 px-3 max-h-[300px]"
+                      dangerouslySetInnerHTML={{
+                        __html: mdParser.render(waiver.content),
+                      }}
+                    />
+                    <button
+                      className="flex gap-1 self-start items-center text-mbb-pink fill-mbb-pink font-semibold"
+                      onClick={() => {
+                        // TODO: Download form
+                      }}
+                    >
+                      <span>Download Form</span> <DownloadIcon />
+                    </button>
+                  </div>
+                }
+              />
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
