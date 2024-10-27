0d62f82d53e124aff27217839484ee51c4f027e6
Uma Anand <uma2005anand@gmail.com>
2024-10-27 18:06:23 -0400

    feat: set new password page at resetpassword web and mobile

--- web/pages/resetPassword.tsx
+++ web/pages/resetPassword.tsx
@@ -14,20 +14,21 @@ export default function ForgotPasswordScreen() {
           <div className="flex flex-col h-full sm:flex-row">
             <HalfScreen />
             <div className="flex flex-col justify-center m-6 mt-8 gap-10 sm:w-1/2 sm:items-center">
-              <div className="flex flex-col gap-3 justify-center sm:items-center">
-                <div className="text-2xl font-bold">Forgot Password?</div>
-                <div>No problem, we'll send you reset instructions.</div>
+              <div className="flex flex-col gap-3 sm:items-center">
+                <div className="text-2xl font-bold">Set New Password?</div>
+                <div>Password must be at least 8 characters</div>
               </div>
               <div className="flex flex-col gap-5 max-w-lg w-full">
-                <TextInput label="Email" />
-                <Button
-                  text="Reset Password"
-                  onClick={() => {
-                    router.push("/resetPassword");
-                  }}
-                ></Button>
+                <TextInput
+                  label="New Password"
+                  placeholder="Create a secure password"
+                />
+                <TextInput
+                  label="Confirm New Password"
+                  placeholder="Confirm your password"
+                />
+                <Button text="Reset Password"></Button>
               </div>
-
               <button
                 className="flex leading-tight tracking-tight items-center gap-1 justify-center"
                 onClick={() => {
