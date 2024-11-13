666bc81f5cdc2823fa31b0eef36fda0a4970cca8
Parker Abramson <parkerrabramson@gmail.com>
2024-11-13 18:31:25 -0500

    Connect baby book page to frontend (first draft)

--- web/components/BabyBook/BabyModal.tsx
+++ web/components/BabyBook/BabyModal.tsx
@@ -1,70 +1,108 @@
 import { useState } from "react";
-import TextInput from "@components/atoms/TextInput";
+import { uploadPhoto } from "db/actions/caregiver/Photo";
 
+import TextInput from "@components/atoms/TextInput";
+import Button from "@components/atoms/Button";
 
 interface Props {
-    image: string;
-    description?: string;
-    edit: true | false
-    deselect?: () => void;
-    selectImage?: (
-      arg0: number,
-      arg1: number,
-      arg2: number,
-      arg3: boolean
-    ) => void;
-    currentIndexs?: {
-      i: number;
-      j: number;
-      k: number;
-    };
-  }
+  image: File | string;
+  description?: string;
+  edit: boolean;
+  caregiverId: string;
+  babyId: string;
+  showBabyModal: (arg0: boolean) => void;
+}
 
-export default function BabyModal({ image, description, edit}: Props) {
+// TODO add edit and delete button (ask Annie for designs if not already)
+export default function BabyModal({
+  image,
+  description,
+  edit,
+  caregiverId,
+  babyId,
+  showBabyModal
+}: Props) {
+  const [newDescription, setNewDescription] = useState("");
+  const [photo, setPhoto] = useState<string>(
+    typeof image === "string" ? image : URL.createObjectURL(image)
+  );
+  const [photoFile, setPhotoFile] = useState<File | undefined>(
+    typeof image === "object" ? image : undefined
+  );
 
-    const [newDescription, setNewDescription] = useState("");
+  return (
+    <div className="flex flex-col w-full h-full items-center sm:flex-row">
+      <div
+        className={`relative w-full bg-image-gray sm:w-[55%] sm:h-full ${edit ? "flex justify-center items-center" : "flex justify-center h-[65%] sm:items-center"}`}
+      >
+        <div className="sm:flex sm:flex-col">
+          <img
+            src={photo}
+            className={`sm:w-[21rem] sm:max-h-[30rem] ${edit ? "max-h-[22rem]" : "w-4/5 object-cover"}`}
+          ></img>
+          {edit && (
+            <label className="bg-primary-text/70 rounded-[999px] text-white text-sm font-semibold py-1 px-3 mt-5 self-end sm:static absolute bottom-3 right-3 cursor-pointer">
+              <input
+                className="hidden"
+                type="file"
+                onChange={(e) => {
+                  const files = e.target.files;
 
-    return (
-        <div className="flex flex-col w-full h-full items-center sm:flex-row">
-            <div className={`relative w-full bg-image-gray sm:w-[55%] sm:h-full ${edit ? "flex justify-center items-center" : "flex justify-center h-[65%] sm:items-center"}`}>
-                <img
-                    src={image}
-                    className={`sm:w-[21rem] sm:max-h-[30rem] ${edit ? "max-h-[22rem]" : "w-4/5 object-cover"}`}
-                ></img>
-                {edit && (
-                    <button className="w-[7.563rem] h-[1.688rem] bg-black/70 rounded-[999px] absolute bottom-3 right-3 sm:bottom-8 sm:right-[10.5rem]">
-                        <div className="w-[6.188rem] h-5 text-white text-sm font-semibold ml-3">
-                            Replace Image
-                        </div>
-                    </button>
-                )}
-            </div>
-            {edit ? (
-                <div className="flex-col w-[20.625rem] h-[14.938rem] justify-start items-start mt-[1.313rem] gap-[1.313rem] inline-flex sm:w-[45%] sm:pl-12 sm:pr-12">
-                    <div className="self-stretch text-lg font-bold sm:text-2xl">
-                        Add a Description
-                    </div>
-                    <div className="w-full h-32">                       
-                        <TextInput
-                            onChange={(event) => {
-                                setNewDescription(event);
-                            }}
-                            placeholder="How the baby is doing, what they did today, etc."
-                        ></TextInput>
-                    </div>
-                    <button className="self-stretch h-[2.813rem] px-4 pt-2 pb-[0.563rem] bg-white rounded border border-mbb-pink justify-center items-center gap-2 inline-flex">
-                        <div className="text-mbb-pink font-semibold sm:text-xl">
-                            Upload
-                        </div>
-                    </button>
-                </div>
-            ) : (
-                <div className="flex flex-col justify-center items-center mx-7 my-7 sm:w-[40%] sm:text-2xl">
-                    <div className="sm:w-2/3">
-                        {description}
-                    </div>
-                </div>
-            )}
+                  if (files) {
+                    const photoFile = files[files.length - 1];
+                    setPhoto(URL.createObjectURL(photoFile));
+                    setPhotoFile(photoFile);
+                  } else {
+                    // TODO show error
+                  }
+                }}
+              />
+              Replace Image
+            </label>
+          )}
         </div>
-    )
+      </div>
+      {edit ? (
+        <div className="flex-col w-[20.625rem] h-[14.938rem] justify-start items-start mt-[1.313rem] gap-[1.313rem] inline-flex sm:w-[45%] sm:pl-12 sm:pr-12">
+          <div className="self-stretch text-lg font-bold sm:text-2xl">
+            Add a Description
+          </div>
+          <div className="w-full h-32">
+            {/* TODO convert to text area */}
+            <TextInput
+              onChange={(event) => {
+                setNewDescription(event);
+              }}
+              placeholder="How the baby is doing, what they did today, etc."
+            ></TextInput>
+          </div>
+          {/* TODO add "Allow for Media Release checkbox" */}
+          <Button
+            text="Upload"
+            onClick={async () => {
+              if (photoFile) {
+                const results = await uploadPhoto({
+                  file: photoFile,
+                  caption: newDescription,
+                  babyId,
+                  caregiverId,
+                });
+                debugger
+
+                if (results.success) {
+                  showBabyModal(false);
+                } else {
+                  // TODO show error
+                }
+              }
+            }}
+          />
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center mx-7 my-7 sm:w-[40%] sm:text-2xl">
+          <div className="sm:w-2/3">{description}</div>
+        </div>
+      )}
+    </div>
+  );
 }
--- web/db/actions/caregiver/Photo.ts
+++ web/db/actions/caregiver/Photo.ts
@@ -1,6 +1,5 @@
-import React from "react";
 import { db, storage } from "../../firebase"; // import firebase storage
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { doc, setDoc, Timestamp } from "firebase/firestore";
 
 interface returnType {
@@ -9,69 +8,53 @@ interface returnType {
   error?: string;
 }
 
-export function uploadPhoto(
-  e: React.ChangeEvent<HTMLInputElement>
-): returnType {
-  const files = e.target.files;
-  if (!files || files.length === 0) {
-    return { success: false, error: "File attempted to be uploaded was empty" };
-  }
+interface Props {
+  file: File;
+  caption: string;
+  babyId: string;
+  caregiverId: string;
+}
 
+export async function uploadPhoto({
+  file,
+  caption,
+  babyId,
+  caregiverId,
+}: Props) {
   try {
-    const file = files[0];
-
     const extension = file.name.split(".").pop();
     const storageRef = ref(storage, `images/${Date.now()}.${extension}`);
 
-    const uploadTask = uploadBytesResumable(storageRef, file);
+    return await uploadBytes(storageRef, file)
+      .then(async (snapshot) => {
+        const downloadURL = await getDownloadURL(snapshot.ref);
 
-    uploadTask.on(
-      "state_changed",
-      (error) => {
-        return { success: false, error: `Upload failed: ${error}` };
-      },
-      async () => {
-        const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
-        // TODO: Incorporate baby and caregiver context
-        // https://github.com/GTBitsOfGood/motherhood-beyond-bars/blob/_original/mobile/screens/babybook/SelectPicture.tsx#L28
-        // TODO: Incorporate caption
-        const metadata = {
-          imageURL: downloadURL,
-          caregiverID: "test-caregiverId",
-          caption: "test-caption",
-          babyID: "test-babyId",
-        };
-
-        const { imageURL, caregiverID, caption, babyID } = metadata;
-
+        // FIX populating baby book bc not working
         const docRef = doc(
           db,
           "babies",
-          babyID,
+          babyId,
           "book",
-          `${caregiverID}_${Date.now()}`
+          `${caregiverId}_${Date.now()}`
         );
 
         // TODO fix logic so that caption is added after
         try {
           await setDoc(docRef, {
-            imageUrl: imageURL,
+            imageUrl: downloadURL,
             caption: caption,
             date: Timestamp.now(),
-            caregiverId: caregiverID,
+            caregiverId: caregiverId,
           });
         } catch (error) {
           return { success: false, error: `Upload failed: ${error}` };
         }
 
         return { success: true, data: { downloadURL: downloadURL } };
-      }
-    );
-
-    return {
-      success: false,
-      error: `Upload failed: Something has gone wrong, please try again`,
-    };
+      })
+      .catch((error) => {
+        return { success: false, error: `Upload failed: ${error}` };
+      });
   } catch (error) {
     return { success: false, error: `Upload failed: ${error}` };
   }
