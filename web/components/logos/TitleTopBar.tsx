a7f4c880c803f519baf0d0310ed53ab462d384d8
Ivan Wang <ivanwang1239@gmail.com>
2025-09-23 20:28:00 -0400

    Fix alignment of resource page and overall overflow

--- web/components/logos/TitleTopBar.tsx
+++ web/components/logos/TitleTopBar.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export default function TitleTopBar({ title }: Props) {
   return (
-    <div className="hidden sm:flex flex-row justify-center items-center h-[4.125rem] bg-custom-background w-full sm:h-[5.1rem]">
+    <div className="hidden sm:flex flex-row justify-center items-center h-[4.125rem] bg-custom-background w-full sm:h-[5rem] sm:min-h-[5rem]">
       <div className="w-full text-center text-white text-lg font-semibold sm:text-2xl">
         {title}
       </div>
--- web/components/resources/ContactInfo.tsx
+++ web/components/resources/ContactInfo.tsx
@@ -1,6 +1,6 @@
 export default function ContactInfo() {
   return (
-    <div className="w-[20.438rem] h-[23.25rem] px-6 py-6 mt-3 bg-white rounded shadow flex-col justify-start items-start inline-flex sm:w-[28.813rem] sm:h-[27.313rem]">
+    <div className="w-[20.438rem] h-[23.25rem] px-6 py-6 mt-3 bg-white rounded shadow flex-col justify-start items-start inline-flex sm:w-full sm:h-full sm:overflow-auto">
       <img src="/MessageTextIcon.svg"></img>
       <div className="w-full text-lg font-bold py-2">Reach out to us!</div>
       <div className="w-full text-dark-gray">
@@ -8,25 +8,25 @@ export default function ContactInfo() {
         if you have any questions, concerns, feedback, or just want to chat.
       </div>
       <div className="flex flex-row pt-3 sm:pt-5">
-        <img src="/CellphoneIcon.svg"></img>
-        <div className="flex flex-col justify-center items-start pl-6">
+        <img src="/CellphoneIcon.svg" className="w-10 h-10"></img>
+        <div className="flex flex-col justify-center items-start pl-6 w-full min-w-0">
           <div className="text-dark-gray text-sm font-semibold">Text</div>
-          (678) 404-1397
+          <div className="break-words w-full">(678) 404-1397</div>
         </div>
       </div>
-      <div className="flex flex-row pt-3 sm:pt-5">
-        <img src="/MailIcon.svg"></img>
-        <div className="flex flex-col justify-center items-start pl-6">
+      <div className="flex flex-row pt-3 sm:pt-5 w-full">
+        <img src="/MailIcon.svg" className="w-10 h-10"></img>
+        <div className="flex flex-col justify-center items-start pl-6 w-full min-w-0">
           <div className="text-dark-gray text-sm font-semibold">Email</div>
-          info@motherhoodbeyond.org
+          <div className="break-words w-full">info@motherhoodbeyond.org</div>
         </div>
       </div>
       <div className="flex flex-row pt-3 sm:pt-5">
-        <img src="/PhoneIcon.svg"></img>
-        <div className="flex flex-col justify-center items-start pl-6">
-          <button className="h-[2.813rem] px-4 rounded border border-mbb-pink justify-center items-center">
+        <img src="/PhoneIcon.svg" className="w-10 h-10"></img>
+        <div className="flex flex-col justify-center items-start pl-6 w-full min-w-0">
+          <button className="h-full px-4 rounded border border-mbb-pink justify-center items-center">
             <div className="text-mbb-pink font-semibold">
-              Call on Google Voice
+              <div className="break-words w-full">Call on Google Voice</div>
             </div>
           </button>
         </div>
